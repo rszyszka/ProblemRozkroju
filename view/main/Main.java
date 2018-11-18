ffd2c52f9f9f262104c381ee3fc03a4e56633d19
rszyszka <sysecka@gmail.com>
2018-11-18 20:56:45 +0100

    initial commit

---
+++ view/main/Main.java
@@ -0,0 +1,20 @@
+package view.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../View.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Problem rozkroju");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
